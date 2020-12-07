7138f9cb519c6f5e9717fba482b75956ee928a7a
nirbhayChaplot <nirbhay.10@chaplot.com>
2020-12-08 00:11:49 +0530

    ADD begin

--- FlipFlops/D_FF.v
+++ FlipFlops/D_FF.v
@@ -1,10 +1,11 @@
 module D_FF(d,q,clk);
 
-input wire clk;
-input wire d;
-output reg q;
+    input wire clk;
+    input wire d;
+    output reg q;
 
-always @ (posedge clk) 
-    q <= d;
-
+    always @ (posedge clk) begin
+        q <= d;
+    end
+    
 endmodule
